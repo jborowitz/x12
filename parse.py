6ec939460d8f1322bd110d53b1cd9e70f16f43a6
Jeffrey Borowitz <borowitz@econ.umd.ed>
2011-12-17 17:11:36 -0500

    code for parsing both 6 and 4 to a line
    Not working here...

--- parse.py
+++ parse.py
@@ -33,12 +33,12 @@ import datetime as dt
 #print(startDate)
 #print(endDate)
 
-outfile = open('judd-output.csv','w')
+outfile = open('women-output.csv','w')
 outfile.write('Date,Value\n')
 #for filename, file in outfiles.items():
     #file.writelines('\t\t)\n\tstart = ' + startDate + '}\nx11{}\n\n')
     #a = sp.call(['/home/jborowitz/bin/x12a/x12a', filename])
-filename = '/home/jborowitz/python/series'
+filename = '/home/jborowitz/python/women'
 f = open(filename + '.out','r')
 line = f.readline()
 year = -1
@@ -46,25 +46,40 @@ finished = 0
 while line.find('D 11') < 0:
     line = f.readline()
 # Get up to section D 11, the final seasonally adjusted data
-while line.find('D 12') < 0:
+#while line.find('D 12') < 0:
+    #while not line[0:5] = '------':
+        #line = f.readline()
+        # Get to the first headers of the table"
+    #while not line[0:5] = '------':
+        #months1 = line[6:-10].split()
+        #months1 = line[6:-10].split()
+
     while not line[2:6].isdigit():
         line = f.readline()
     year = int(line[2:6])
-    print(year)
+    #print(year)
     yearStartDate = dt.date(year,1,1)
     d=[]
     while len(line) > 10:
         try:
-            d.append(float(line[6:21].strip()))
-            d.append(float(line[22:34].strip()))
-            d.append(float(line[35:47].strip()))
-            d.append(float(line[48:60].strip()))
+            #d.append(float(line[6:21].strip()))
+            #d.append(float(line[22:34].strip()))
+            #d.append(float(line[35:47].strip()))
+            #d.append(float(line[48:60].strip()))
+            
+            d.extend(line[6:-10].split())
+            #d.append(float(line[6:18].strip()))
+            #d.append(float(line[19:27].strip()))
+            #d.append(float(line[28:36].strip()))
+            #d.append(float(line[37:45].strip()))
+            #d.append(float(line[46:54].strip()))
+            #d.append(float(line[55:63].strip()))
             line = f.readline()
         except ValueError: 
             while line.find('D 12') < 0:
-                print(line)
+                #print(line)
                 line = f.readline()
-            print(line)
+            #print(line)
             #finished = 1
             #print(line)
             #line = f.readline()
