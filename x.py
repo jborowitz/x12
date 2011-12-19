29f627b9d38b2ee66c6d132020f6289de3aafefb
Jeffrey Borowitz <borowitz@econ.umd.ed>
2011-12-18 20:37:23 -0500

    combined x and parse + fixed subprocess

--- x.py
+++ x.py
@@ -3,6 +3,7 @@
 #import datetime as dt
 import csv, os, subprocess, sys, time
 from datetime import datetime
+import datetime as dt
 
 outfiles = {}
 times = []
@@ -22,30 +23,40 @@ for line in a:
             outfiles[field].write('\t\t' + v + '\n')
 
 times.sort()
-startDate = times[1].strftime('%Y.%m')
+startDate = times[0].strftime('%Y.%m')
 endDate = times[len(times)-1]
+maxyear = endDate.year
+print(maxyear)
 
 for filename, file in outfiles.items():
-    file.writelines('\t\t)\n\tstart = ' + startDate + '}\nx11{}\n\n')
-    #string = ['/home/jborowitz/bin/x12a/x12a' , filename]
-    string = 'nohup /home/jborowitz/bin/x12a/x12a ' + filename + ' &'
-    print(string)
-    #a = subprocess.Popen(string)
-    #a.communicate()
-    a = subprocess.Popen(string,shell=True)
-    #a = subprocess.call(string)
-    
-    #a.wait()
-    #a.wait()
-    #print(a.returncode)
-    #a.terminate()
-    #while a.poll() is None:
-    #time.sleep(3)
-        #print(time.time())
-    #a.wait()
-    #a.communicate() #will not pass this line until the process above terminates
+    file.writelines('\t\t)\n\tstart = ' + startDate + '\n\tdecimals = 2}\nx11{}\n\n')
+    file.close()
+    subprocess.call(['/home/jborowitz/bin/x12a/x12a',filename])
 
-    #a = sp.call(['/home/jborowitz/bin/x12a/x12a', filename])
-    #a = sp.call(string, shell=True)
-    #print(a)
-    
+
+for filestub in outfiles.iterkeys():
+    x12out = open(filestub + '.out','r')
+    line = x12out.readline()
+    year = -1
+    outputcsv = open(filestub + '.csv','w')
+    outputcsv.write('date,' + filestub + '\n')
+    while line.find('D 11') < 0:
+        line = x12out.readline()
+        #print(line)
+
+    while year < maxyear:
+        while not line[2:6].isdigit():
+            line = x12out.readline()
+        year = int(line[2:6])
+        d=[]
+        while len(line) > 10:
+            st = line[9:66].split()
+            d.extend(st)
+            line = x12out.readline()
+        for index in range(len(d)):
+            if year < maxyear:
+                monthDate = dt.date(year,index + 13 - len(d),1)
+            else:
+                monthDate = dt.date(year,index + 1,1)
+            dstring = monthDate.strftime('%Y-%m')
+            outputcsv.write(dstring + ',' + str(d[index]) + '\n')
